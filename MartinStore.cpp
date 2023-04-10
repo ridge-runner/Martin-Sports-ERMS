6f10c21fd340f5e17159f96cfa58e7cd2223536a
Bvandyke <bvandyke00@gmail.com>
2023-04-10 12:28:25 -0400

    Add project files.

---
+++ MartinStore.cpp
@@ -0,0 +1,99 @@
+// Ch10.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <stdlib.h> // win10 system calls: system();
+
+using namespace std;
+
+void displayHeader();
+void displayMainMenu();
+void displaySales(double, double, double, double);
+void gotoMain();
+
+int main()
+{
+	double sales0 = 0.0;
+	double sales1 = 0.0;
+	double sales2 = 0.0;
+	double sales3 = 0.0;
+
+	double salesTotal = 0.0;
+	
+	salesTotal = sales0 + sales1 + sales2 + sales3;
+	
+	int exit = 0;
+	while (exit == 0)
+	{
+
+	// UI
+	displayHeader();
+	
+	// Main Menu
+	displayMainMenu();
+	cout << ">>";
+
+	int menuChoice = 0;
+	cin >> menuChoice;
+	
+	// Main Menu Swtich
+	switch (menuChoice)
+	{
+	case 1: 
+		displaySales(sales0, sales1, sales2, sales3);
+		break;
+	default: 
+		cout << "Sorry, currently under development.";
+	} // end Switch
+
+	cout << "\n\nExit ERMS? (Press 1 to exit) >> ";
+	cin >> exit;
+
+	} // end MainLoop
+	
+}
+
+void displayHeader()
+{
+	cout << "-----------------------------" << endl
+		<< "MARTIN SPORTS" << endl
+		<< "Atlanta, GA" << endl
+		<< "-----------------------------"<< endl << endl;
+
+}
+
+void displayMainMenu()
+{
+	cout << "Please choose one of the following options:" << endl
+		<< "1. Sales" << endl
+		<< "2. Personel" << endl
+		<< "3. Expense Reports" << endl;
+}
+
+void displaySales(double sales0, double sales1, double sales2, double sales3)
+{
+	system("cls"); // Clear screen
+
+	displayHeader();
+	// sales page data report
+	cout << endl <<
+		"Sales Report for all Stores:" << endl <<
+		"1. Anchor Store: $" << sales0 << endl <<
+		"2. Ridgewood: $" << sales1 << endl <<
+		"3. Phiffer: $" << sales2 << endl <<
+		"4. Mall Kiosk: $" << sales3 << endl;
+}
+
+void gotoMain() 
+{
+	char toMain = 'Y';
+	while (toMain == toupper('Y'))
+	{
+		cout << "Back to main menu? (Y/N) ";
+		cin >> toMain;
+	}
+
+	system("CLS");
+}
---
+++ README.txt
@@ -0,0 +1,71 @@
+
+README
+
+Pseudocode for Martin Sports 
+
+IPO:
+- directives - 
+rand
+srand
+ctime
+iomanip
+
+
+MAIN():
+	--VARIABLES----
+	double salesA
+	double salesB
+	double salesTotal
+	
+
+void displayTotalSales(double totalSales)
+
+
+------------------------------------
+---------- SCRATCH -----------------
+
+1. Display main menu options.
+	a. Sales
+	b. Costs <void>
+	c. Expenses <void>
+
+2. <a> selected.
+
+3. Display Sales Menu.
+	a. Main store  #0	$xxx
+	b. Chain store #1	$xxx
+	c. Chain store #2	$xxx
+	d. Chain store #3	$xxx
+
+	e. Total sales		$xxx
+	
+	f. Sales breakdown ...
+
+4. <e> selected
+5. Show breakdown prompt:
+	a. Enter store numbers:<>
+	
+6. Display salesTotals for store nos.
+7. exit program.
+
+----------------------------------
+		// PROMPT Design \\
+----------------------------------
+
+HEADER:
+
+---------------------------------
+		MARTIN SPORTS
+		Atlanta, GA
+---------------------------------
+
+SALES BREAKDOWN REPORT:
+
+Selected stores:
+
+1. Store #1	$xxx
+2. Store #2	$xxx
+-----------------
+Total Sales: $xxx
+
+
